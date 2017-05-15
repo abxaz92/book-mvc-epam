96098c3e52ce53c9975a799ebd35fc40d29cede7
David_Chaava <David_Chaava@epam.com>
2017-05-15 17:53:50 +0300

    spring data jpa postgresql

--- src/main/resources/create-db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE books (
-  id         INTEGER not null primary key
-        GENERATED ALWAYS AS IDENTITY
-        (START WITH 1, INCREMENT BY 1),
-  name VARCHAR(100),
-  author  VARCHAR(100)
-);
--- src/main/resources/insert-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO books (name, author) VALUES ('Война и Мир', 'Толстой');
-INSERT INTO books (name, author) VALUES ('Мастер и Маргарита', 'Булгаков');
-INSERT INTO books (name, author) VALUES ('После Бала', 'Толстой');
